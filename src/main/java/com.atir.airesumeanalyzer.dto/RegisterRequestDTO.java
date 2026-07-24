e74ad33b6bcea9efb4632df65a91bd71915d3781
Atir Suhail <atirsuhail406@gmail.com>
2026-07-24 10:41:50 +0530

    RegisterRequestDTO.java

---
+++ src/main/java/com.atir.airesumeanalyzer.dto/RegisterRequestDTO.java
@@ -0,0 +1,44 @@
+package com.atir.airesumeanalyzer.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequestDTO {
+	@NotBlank(message = "First Name is required")
+    private String firstName;
+	@NotBlank(message = "Last Name is required")
+    private String lastName;
+	@Email(message = "Invalid email format")
+	@NotBlank(message = "Email is required")
+    private String email;
+	@NotBlank(message = "Password is required")
+	@Size(min = 8, message = "Password must be at least 8 characters")
+	private String password;
+	public RegisterRequestDTO() {}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
