c8dfebfa561e9eecbaf54443c74b755a6baa644a
Atir Suhail <atirsuhail406@gmail.com>
2026-07-24 10:45:19 +0530

    EmailAlreadyExistsException.java

---
+++ src/main/java/com.atir.airesumeanalyzer.exception/EmailAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.atir.airesumeanalyzer.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+	public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+	
+
+}
