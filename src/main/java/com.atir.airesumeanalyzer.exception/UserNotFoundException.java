9c893e9791f5e3189c539d131d31f257231c7cfd
Atir Suhail <atirsuhail406@gmail.com>
2026-07-24 10:47:14 +0530

    UserNotFoundException.java

---
+++ src/main/java/com.atir.airesumeanalyzer.exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.atir.airesumeanalyzer.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
